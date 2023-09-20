ca1022148b2fc77d61f7fcf661b0a1abc831d0ad
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2023-09-20 12:53:39 +0200

    Improve test data arrangement in test code

--- test/LemmatchersSpec.hs
+++ test/LemmatchersSpec.hs
@@ -11,22 +11,12 @@ spec = describe "Lemmatchers tests" $ do
 simpleExample :: Spec
 simpleExample = describe "Simple example" $ do
 
-  let matchersString =  "matcher Foo Bar\n\
-                        \PN N DET\n\
-                        \\n\
-                        \---\n\
-                        \\n\
-                        \matcher Baz Quux\n\
-                        \-N (PN PRP)\n\
-                        \-(DN AV) * V\n"
-
-      p1 = Pattern [Exact PN, Exact N, Exact DET]
-      p2 = Pattern [Not N, OneOf [PN, PRP]]
-      p3 = Pattern [NoneOf [DN, AV], Any, Exact V]
-      m1 = Matcher "Foo Bar" [p1]
-      m2 = Matcher "Baz Quux" [p2, p3]
-      matchers = Matchers [m1, m2]
-
+  let lemmaDataCsv =
+        "id_text,Designation,lemma\r\n\
+        \1,d1,a[b]NA\r\n\
+        \2,d2,c[d]MOD e[f]PN g[h]N i[j]DET k[l]DET\r\n\
+        \3,d3,m[n]MOD o[p]PRP q[r]NU s[t]REL u[v]V\r\n\
+        \"
       lr1 = LR "1" "d1" (Lemmas [ Lemma "a" "b" NA ] )
       lr2 = LR "2" "d2" (Lemmas [ Lemma "c" "d" MOD
                                 , Lemma "e" "f" PN
@@ -40,6 +30,22 @@ simpleExample = describe "Simple example" $ do
                                 , Lemma "s" "t" REL
                                 , Lemma "u" "v" V
                                 ] )
+      lemmaRecords = [lr1, lr2, lr3]
+
+      matchersString =  "matcher Foo Bar\n\
+                        \PN N DET\n\
+                        \\n\
+                        \---\n\
+                        \\n\
+                        \matcher Baz Quux\n\
+                        \-N (PN PRP)\n\
+                        \-(DN AV) * V\n"
+      p1 = Pattern [Exact PN, Exact N, Exact DET]
+      p2 = Pattern [Not N, OneOf [PN, PRP]]
+      p3 = Pattern [NoneOf [DN, AV], Any, Exact V]
+      m1 = Matcher "Foo Bar" [p1]
+      m2 = Matcher "Baz Quux" [p2, p3]
+      matchers = Matchers [m1, m2]
 
       mm1lr1  = matchRecord m1 lr1
       mm1lr2  = matchRecord m1 lr2
@@ -49,6 +55,14 @@ simpleExample = describe "Simple example" $ do
       mm2lr3  = matchRecord m2 lr3
       matches = concat [mm1lr1, mm1lr2, mm1lr3, mm2lr1, mm2lr2, mm2lr3]
       
+      resultsCsv =
+        "matcher,pattern,id_text,Designation,lemma\r\n\
+        \Foo Bar,PN N DET,2,d2,e[f]PN g[h]N i[j]DET\r\n\
+        \Baz Quux,-N (PN PRP),2,d2,c[d]MOD e[f]PN\r\n\
+        \Baz Quux,-N (PN PRP),3,d3,m[n]MOD o[p]PRP\r\n\
+        \Baz Quux,-(DN AV) * V,3,d3,q[r]NU s[t]REL u[v]V\r\n\
+        \"
+
   context "Stringification" $ do
     it "Parse correct Matchers" $
       read matchersString `shouldBe` matchers
@@ -74,21 +88,7 @@ simpleExample = describe "Simple example" $ do
         ]
 
   context "CSV import and export" $ do
-
     it "Correct records parsing from CSV" $
-      csvToLemmaRecords
-        "id_text,Designation,lemma\r\n\
-        \1,d1,a[b]NA\r\n\
-        \2,d2,c[d]MOD e[f]PN g[h]N i[j]DET k[l]DET\r\n\
-        \3,d3,m[n]MOD o[p]PRP q[r]NU s[t]REL u[v]V\r\n\
-        \"
-        `shouldBe` [lr1, lr2, lr3]
-
+      csvToLemmaRecords lemmaDataCsv `shouldBe` lemmaRecords
     it "Correct matching results CSV export" $
-      matchesToCsv matches `shouldBe`
-        "matcher,pattern,id_text,Designation,lemma\r\n\
-        \Foo Bar,PN N DET,2,d2,e[f]PN g[h]N i[j]DET\r\n\
-        \Baz Quux,-N (PN PRP),2,d2,c[d]MOD e[f]PN\r\n\
-        \Baz Quux,-N (PN PRP),3,d3,m[n]MOD o[p]PRP\r\n\
-        \Baz Quux,-(DN AV) * V,3,d3,q[r]NU s[t]REL u[v]V\r\n\
-        \"
+      matchesToCsv matches `shouldBe` resultsCsv
