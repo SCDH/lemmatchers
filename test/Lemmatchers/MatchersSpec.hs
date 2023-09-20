6546925103a28c478461e84c9981314af1a02971
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2023-09-20 12:40:24 +0200

    Add complete functionality example in one test file

--- test/Lemmatchers/MatchersSpec.hs
+++
@@ -1,6 +0,0 @@
-module Lemmatchers.MatchersSpec where
-
-import Test.Hspec
-
-spec :: Spec
-spec = it "dummy-test" $ 42 == 42
--- test/Lemmatchers/TagRecordsSpec.hs
+++
@@ -1,8 +0,0 @@
-module Lemmatchers.TagRecordsSpec where
-
-import Test.Hspec
-import Test.Hspec.QuickCheck
-import Test.QuickCheck
-
-spec :: Spec
-spec = prop "dummy-property" $ \i -> i > 0 ==> abs i === (i :: Int)
