ebb49c77be7712ad40b857bf949a70f9bf0cd209
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2023-09-29 13:21:32 +0200

    Add lemma matching to web service

--- web/Main.hs
+++ web/Main.hs
@@ -1,30 +1,42 @@
 module Main where
 
+import Lemmatchers.Matchers
+import Lemmatchers.TagRecords
+import qualified Data.Text as T
+import Data.Either.Extra
+import Data.Csv
+import qualified Data.Vector as V
 import Data.Proxy
-import Data.Text (Text)
+import Text.Read
 import Text.Blaze.Html5 (Html)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Servant.API
 import Servant
 import Servant.HTML.Blaze
+import Servant.Multipart
+import Servant.CSV.Cassava
 import Text.Blaze
 import Network.Wai.Handler.Warp
-import Web.FormUrlEncoded
-import GHC.Generics
 import Control.Monad.IO.Class
 
-data MatcherForm = MatcherForm
-  { dummy       :: Text
-  , mfMatchers  :: Text
-  } deriving (Eq, Show, Generic)
+data MatcherFormData = MFD
+  { mfMatchers      :: Matchers
+  , mfLemmaRecords  :: [LemmaRecord]
+  } deriving Show
 
-instance FromForm MatcherForm
+instance FromMultipart Mem MatcherFormData where
+  fromMultipart mpd = do
+    msS <- T.unpack <$> lookupInput "matchers" mpd
+    ms  <- maybeToEither ("Unable to parse matchers: " ++ msS) $ readMaybe msS
+    csv <- fdPayload <$> lookupFile "lemmasCSV" mpd
+    lrs <- V.toList . snd <$> decodeByName csv
+    return $ MFD ms lrs
 
 type LemmatchersAPI = FormAPI :<|> MatchingAPI
 type FormAPI        = Get '[HTML] Html
-type MatchingAPI    = "match" :> ReqBody '[FormUrlEncoded] MatcherForm
-                              :> Post '[HTML] Html
+type MatchingAPI    = "match" :> MultipartForm Mem MatcherFormData
+                              :> Post '[CSV] [Match]
 
 lemmatchersAPI  :: Proxy LemmatchersAPI
 formAPI         :: Proxy FormAPI
@@ -44,17 +56,16 @@ handleForm = do
     H.h1 "Lemmatchers"
     H.form  ! A.action (textValue $ toUrlPiece $ safeLink lemmatchersAPI matchingAPI)
             ! A.method "post"
+            ! A.enctype "multipart/form-data"
             $ do
       H.fieldset $ do
         H.legend "Input data"
         H.p "Please provide the input data as a CSV file:"
-        H.p $ H.input ! A.type_ "text"
-                      ! A.id    "dummy"
-                      ! A.name  "dummy"
+        H.p $ H.input ! A.type_ "file"
+                      ! A.name  "lemmasCSV"
       H.fieldset $ do
         H.legend "Matcher rules"
-        H.textarea  ! A.id    "mfMatchers"
-                    ! A.name  "mfMatchers"
+        H.textarea  ! A.name  "matchers"
                     ! A.rows  "15"
                     ! A.cols  "30"
                     $ H.string defaultMatchers
@@ -64,8 +75,9 @@ handleForm = do
         H.p $ H.input ! A.type_ "submit"
                       ! A.value "May the matching begin!"
 
-handleMatching :: MatcherForm -> Handler Html
-handleMatching mf = undefined
+handleMatching :: MatcherFormData -> Handler [Match]
+handleMatching (MFD ms lrs) =
+  return $ concat [matchRecord m lr | m <- matchers ms, lr <- lrs]
 
 main :: IO ()
 main = do
